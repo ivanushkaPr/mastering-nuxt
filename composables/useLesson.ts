35a7b39fbdd4f4c1e803ed65e15c3614df82a114
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2023-09-13 22:46:46 +0300

    5.6. ended chapter.

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,17 +1,36 @@
+import { StorageSerializers } from '@vueuse/core';
+import { LessonWithPath } from '~/types/course';
+
 export default async (
     chapterSlug: string,
     lessonSlug: string,
 )=> {
-    const { data, error } = await useFetch(
-        `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
-    )
 
-    if (error.value) {
-        throw createError({
-            ... error.value,
-            statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`
-        });
+    const url = `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`;
+    const lesson = useSessionStorage<LessonWithPath>(
+        url,
+        null,
+        {
+            serializer: StorageSerializers.object,
+        }
+    );
+
+    if (!lesson.value) {
+        const { data, error } = await useFetch<LessonWithPath>(
+            url,
+        );
+
+        if (error.value) {
+            throw createError({
+                ... error.value,
+                statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`
+            });
+        }
+
+        lesson.value = data.value;
+    } else {
+        console.log(`Getting lesson ${lessonSlug} in chapter ${chapterSlug} from cache`);
     }
 
-    return data;
+    return lesson;
 }
